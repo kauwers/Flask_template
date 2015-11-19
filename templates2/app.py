19053f4d26f40876a86dd55b41c834126ad29627
deeeex <greedland1023@gmail.com>
2015-11-19 17:34:29 -0500

    Revert "Merge pull request #1 from kauwers/test-branch"
    This reverts commit 56c9bdaa2f43df2e672944f7e9b2f826e36210ae, reversing
    changes made to aff27c4105b2a5e9a317ca85f1db83ddb22e81bd.

--- templates2/app.py
+++ templates2/app.py
@@ -9,18 +9,18 @@ app = Flask(__name__)    #This is creating a new Flask object
 def index():
     return render_template("index.html", name="index", title="Home")		#The argument should be in templates folder
 
-@app.route('/interests')
+@app.route('/interests')   
 def interests():
     return render_template("interests.html", name="interests", title="Interests")
 
 #Add the code for courses
 
-@app.route('/courses')
+@app.route('/courses')   
 def courses():
     return render_template("courses.html", name="courses", title="Courses")
 
 #Add the code for other
-@app.route('/other')
+@app.route('/other')   
 def other():
     return render_template("other.html", name="other", title="Other")
 
@@ -29,4 +29,4 @@ def other():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4500)		#debug=True is optional
+    app.run(debug=True)		#debug=True is optional
